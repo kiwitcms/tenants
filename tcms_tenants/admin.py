34b9b1dd4c49f9d05ee375415f1808db0a8fd533
Mr. Senko <atodorov@mrsenko.com>
2019-03-27 22:02:07 +0200

    Show domain name in Tenant admin

--- tcms_tenants/admin.py
+++ tcms_tenants/admin.py
@@ -9,7 +9,7 @@ from tcms_tenants.models import Tenant
 
 class TenantAdmin(admin.ModelAdmin):
     actions = ['delete_selected']
-    list_display = ('id', 'name', 'schema_name', 'created_on', 'on_trial', 'paid_until')
+    list_display = ('id', 'name', 'schema_name', 'domain_name', 'created_on', 'on_trial', 'paid_until')
     search_fields = ('name', 'schema_name')
     ordering = ['-created_on']
 
@@ -25,6 +25,9 @@ class TenantAdmin(admin.ModelAdmin):
             return super().delete_view(request, object_id, extra_context)
 
         return HttpResponseRedirect(reverse('admin:tcms_tenants_tenant_changelist'))
+
+    def domain_name(self, instance):
+        return instance.domains.first().domain
 
 
 admin.site.register(Tenant, TenantAdmin)
