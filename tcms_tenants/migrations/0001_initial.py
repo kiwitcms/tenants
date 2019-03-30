44383b7d5bc943f4d48fd1f70bcf89861ad347b2
Mr. Senko <atodorov@mrsenko.com>
2019-03-30 22:46:10 +0200

    Add db_index for Tenant.schema_name and Domain.is_primary
    see
    https://github.com/tomturner/django-tenants/pull/253

--- tcms_tenants/migrations/0001_initial.py
+++ tcms_tenants/migrations/0001_initial.py
@@ -16,7 +16,8 @@ class Migration(migrations.Migration):
             name='Tenant',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('schema_name', models.CharField(unique=True, max_length=63, validators=[django_tenants.postgresql_backend.base._check_schema_name])),
+                ('schema_name', models.CharField(unique=True, max_length=63, db_index=True,
+                                                 validators=[django_tenants.postgresql_backend.base._check_schema_name])),
                 ('name', models.CharField(max_length=100, db_index=True)),
                 ('created_on', models.DateField(auto_now_add=True)),
                 ('paid_until', models.DateField(null=True, blank=True)),
@@ -31,7 +32,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('domain', models.CharField(unique=True, max_length=253, db_index=True)),
-                ('is_primary', models.BooleanField(default=True)),
+                ('is_primary', models.BooleanField(default=True, db_index=True)),
                 ('tenant', models.ForeignKey(related_name='domains', to='tcms_tenants.Tenant', on_delete=models.deletion.CASCADE)),
             ],
             options={
