1c569e1200e7526bca167d8da3bcb13a62d47f5f
Mr. Senko <atodorov@mrsenko.com>
2019-03-30 21:48:20 +0200

    Automatically drop schema when tenant is removed

--- tcms_tenants/models.py
+++ tcms_tenants/models.py
@@ -8,8 +8,8 @@ from django_tenants.models import TenantMixin, DomainMixin
 
 
 class Tenant(TenantMixin):
-    # schema will be automatically created and synced when it is saved
     auto_create_schema = True
+    auto_drop_schema = True
 
     name = models.CharField(max_length=100, db_index=True)
     created_on = models.DateField(auto_now_add=True)
