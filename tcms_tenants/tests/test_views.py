3f965242ebb8d7f6aee0dffd0a60fe72e1b5f706
Mr. Senko <atodorov@mrsenko.com>
2019-05-04 23:58:03 +0300

    Add on_trial and paid_until fields in form/create_tenant
    render form.on_trial as hidden b/c when not provided Django
    defaults to False and we need the default to be True!
    
    Same for form.paid_until.
    
    Also render them in extra_contents block so they can be overriden
    if desired.

--- tcms_tenants/tests/test_views.py
+++ tcms_tenants/tests/test_views.py
@@ -2,6 +2,8 @@
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 
+from datetime import datetime, timedelta
+
 from django.urls import reverse
 from django.db import connection
 from django.conf import settings
@@ -39,3 +41,59 @@ class RedirectToTestCase(LoggedInTestCase):
 
         self.assertIsInstance(response, HttpResponseRedirect)
         self.assertEqual(response['Location'], expected_url)
+
+
+class NewTenantViewTestCase(LoggedInTestCase):
+    def test_create_tenant_shows_defaults_for_trial_and_paid_until(self):
+        response = self.client.get(reverse('tcms_tenants:create-tenant'))
+        # assert hidden fields are shown with defaults b/c the tests below
+        # can't simulate opening the page and clicking the submit button
+        self.assertContains(response,
+                            '<input id="id_on_trial" name="on_trial" value="True" type="hidden">',
+                            html=True)
+        self.assertContains(response,
+                            '<input id="id_paid_until" name="paid_until" type="hidden">',
+                            html=True)
+
+    def test_create_tenant_with_name_schema_only(self):
+        expected_url = 'https://tinc.%s' % settings.KIWI_TENANTS_DOMAIN
+        response = self.client.post(
+            reverse('tcms_tenants:create-tenant'),
+            {
+                'name': 'Tenant, Inc.',
+                'schema_name': 'tinc',
+                # this is what the default form view sends
+                'on_trial': True,
+                'paid_until': '',
+            })
+
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertEqual(response['Location'], expected_url)
+
+        tenant = Tenant.objects.get(schema_name='tinc')
+        self.assertTrue(tenant.on_trial)
+        self.assertIsNone(tenant.paid_until)
+
+    def test_create_tenant_with_name_schema_on_trial_payment_date(self):
+        """
+            Similar invocation will be used via inherited view in
+            GitHub Marketplace integration.
+        """
+        expected_url = 'https://subscriber.%s' % settings.KIWI_TENANTS_DOMAIN
+        paid_until = datetime.now() + timedelta(days=30)
+
+        response = self.client.post(
+            reverse('tcms_tenants:create-tenant'),
+            {
+                'name': 'Subscriber LLC',
+                'schema_name': 'subscriber',
+                'on_trial': False,
+                'paid_until': paid_until.strftime('%Y-%m-%d'),  # %H:%M:%S'),
+            })
+
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertEqual(response['Location'], expected_url)
+
+        tenant = Tenant.objects.get(schema_name='subscriber')
+        self.assertFalse(tenant.on_trial)
+        self.assertEqual(tenant.paid_until, paid_until.date())
