d48f6d8dfc5bfee353a16dd1f1e17d3b7e10fb71
Mr. Senko <atodorov@mrsenko.com>
2019-05-08 00:11:18 +0300

    Add middleware which can be used to block unpaid tenants
    it is not enabled by default

--- tcms_tenants/middleware.py
+++ tcms_tenants/middleware.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+# pylint: disable=too-few-public-methods
+from datetime import datetime, timedelta
 
+from django.contrib import messages
 from django.http import HttpResponseForbidden
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.translation import ugettext_lazy as _
@@ -9,14 +12,14 @@ from django.utils.translation import ugettext_lazy as _
 from tcms_tenants.utils import can_access
 
 
-class BlockUnauthorizedUserMiddleware(MiddlewareMixin):  # pylint: disable=too-few-public-methods
+class BlockUnauthorizedUserMiddleware(MiddlewareMixin):
     """
         Raises 404 if the user making the request is not authorized
         explicitly to access the tenant instance!
 
         .. warning:
 
-            This must be places after
+            This must be placed after
             ``django_tenants.middleware.main.TenantMainMiddleware`` and
             ``django.contrib.auth.middleware.AuthenticationMiddleware`` -
             usually goes at the end
@@ -27,3 +30,34 @@ class BlockUnauthorizedUserMiddleware(MiddlewareMixin):  # pylint: disable=too-f
             return HttpResponseForbidden(_('Unauthorized'))
 
         return None
+
+
+class BlockUnpaidTenantMiddleware:
+    """
+        Blocks unpaid tenants or adds warning messages if tenant is about to
+        expire soon!
+
+        .. warning:
+
+            This must be placed after
+            ``tcms_tenants.middleware.BlockUnauthorizedUserMiddleware`` -
+            usually goes at the end.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.tenant.paid_util is None:
+            return HttpResponseForbidden(_('Unauthorized'))
+
+        if request.tenant.paid_util <= datetime.now():
+            return HttpResponseForbidden(_('Unpaid'))
+
+        if request.tenant.paid_util <= datetime.now() + timedelta(days=7):
+            # todo: can we avoid adding message if it already exists ???
+            messages.add_message(request,
+                                 messages.WARNING,
+                                 _('Tenant expires in less than 7 days'),
+                                 fail_silently=True)
+
+        return self.get_response(request)
