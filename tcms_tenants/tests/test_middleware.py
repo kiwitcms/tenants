ddde2336030d0cf7d69b9da86cb21e53251ed447
Mr. Senko <atodorov@mrsenko.com>
2019-05-08 00:42:56 +0300

    Add tests for BlockUnpaidTenantMiddleware

--- tcms_tenants/tests/test_middleware.py
+++ tcms_tenants/tests/test_middleware.py
@@ -2,7 +2,10 @@
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 # pylint: disable=too-many-ancestors
+from datetime import datetime, timedelta
+
 from django.conf import settings
+from django.test import modify_settings
 from django.http import HttpResponseForbidden
 
 from tcms_tenants.tests import LoggedInTestCase
@@ -15,3 +18,52 @@ class BlockUnauthorizedUserMiddlewareTestCase(LoggedInTestCase):
 
         self.assertIsInstance(response, HttpResponseForbidden)
         self.assertIn('Unauthorized', str(response.content, encoding=settings.DEFAULT_CHARSET))
+
+
+class BlockUnpaidTenantMiddlewareTestCase(LoggedInTestCase):
+
+    @modify_settings(MIDDLEWARE={
+        'append': 'tcms_tenants.middleware.BlockUnpaidTenantMiddleware',
+    })
+    def test_paid_until_is_none_access_forbidden(self):
+        self.tenant.paid_until = None
+        self.tenant.save()
+
+        response = self.client.get('/')
+
+        self.assertIsInstance(response, HttpResponseForbidden)
+        self.assertIn('Unpaid', str(response.content, encoding=settings.DEFAULT_CHARSET))
+
+    @modify_settings(MIDDLEWARE={
+        'append': 'tcms_tenants.middleware.BlockUnpaidTenantMiddleware',
+    })
+    def test_paid_until_lt_today_access_forbidden(self):
+        self.tenant.paid_until = datetime.now() - timedelta(days=1)
+        self.tenant.save()
+
+        response = self.client.get('/')
+
+        self.assertIsInstance(response, HttpResponseForbidden)
+        self.assertIn('Unpaid', str(response.content, encoding=settings.DEFAULT_CHARSET))
+
+    @modify_settings(MIDDLEWARE={
+        'append': 'tcms_tenants.middleware.BlockUnpaidTenantMiddleware',
+    })
+    def test_paid_until_expires_within_week_warning_shown(self):
+        self.tenant.paid_until = datetime.now() + timedelta(days=3)
+        self.tenant.save()
+
+        response = self.client.get('/')
+
+        self.assertContains(response, 'Tenant expires in less than 7 days')
+
+    @modify_settings(MIDDLEWARE={
+        'append': 'tcms_tenants.middleware.BlockUnpaidTenantMiddleware',
+    })
+    def test_paid_until_gt_today_access_granted(self):
+        self.tenant.paid_until = datetime.now() + timedelta(days=30)
+        self.tenant.save()
+
+        response = self.client.get('/')
+
+        self.assertContains(response, 'DASHBOARD')
