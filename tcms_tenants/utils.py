0d19c0971c0635819acce1822ec22156f74f9792
Mr. Senko <atodorov@mrsenko.com>
2020-03-03 22:12:31 +0200

    Fix pylint issue

--- tcms_tenants/utils.py
+++ tcms_tenants/utils.py
@@ -110,7 +110,7 @@ def create_oss_tenant(owner, name, schema_name, organization):
         Used to create tenants for our OSS program. Executed by the
         instance administrator!
     """
-    class FakeRequest:
+    class FakeRequest:  # pylint: disable=too-few-public-methods
         is_secure = True
         user = None
 
