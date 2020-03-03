91aa137e0ae44ede3ac94a3f625781d9f7ae425f
Mr. Senko <atodorov@mrsenko.com>
2020-03-03 22:54:49 +0200

    Reimplement NewTenantView as FormView
    WARNING: this is inherited and overriden by
    kiwitcms-github-marketplace !!!
    
    makes code simpler and makes pylint happier

--- tcms_tenants/views.py
+++ tcms_tenants/views.py
@@ -3,10 +3,8 @@
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 
 from django.conf import settings
-from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.template.loader import select_template
-from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
@@ -15,12 +13,14 @@ from tcms_tenants.forms import NewTenantForm
 
 
 @method_decorator(login_required, name='dispatch')
-class NewTenantView(TemplateView):
+class NewTenantView(FormView):
     """
         Everybody is allowed to create new tenants.
         Depending on additional configuration they may have to pay
         for using them!
     """
+    form_class = NewTenantForm
+
     def get_template_names(self):
         """
             Allow downstream installations to override the template. For example
@@ -30,25 +30,14 @@ class NewTenantView(TemplateView):
         return ['tcms_tenants/override_new.html', 'tcms_tenants/new.html']
 
     def get_context_data(self, **kwargs):
-        return {
-            'form': NewTenantForm(),
-            'tcms_tenants_domain': settings.KIWI_TENANTS_DOMAIN,
-        }
+        context = super().get_context_data(**kwargs)
+        context['tcms_tenants_domain'] = settings.KIWI_TENANTS_DOMAIN
+        return context
 
-    def post(self, request, *args, **kwargs):
-        form = NewTenantForm(request.POST)
-        if form.is_valid():
-            tenant = utils.create_tenant(form.cleaned_data, request)
-            # all is successfull so redirect to the new tenant
-            return HttpResponseRedirect(utils.tenant_url(request, tenant.schema_name))
-
-        context_data = {
-            'form': form,
-            'tcms_tenants_domain': settings.KIWI_TENANTS_DOMAIN,
-        }
-        # gets the first template which exists
-        template_name = select_template(self.get_template_names()).template.name
-        return render(request, template_name, context_data)
+    def form_valid(self, form):
+        tenant = utils.create_tenant(form.cleaned_data, self.request)
+        # all is successfull so redirect to the new tenant
+        return HttpResponseRedirect(utils.tenant_url(self.request, tenant.schema_name))
 
 
 @login_required
