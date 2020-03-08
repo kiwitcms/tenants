61c3676a1bc230d06532791e726dc91276ce9e15
Mr. Senko <atodorov@mrsenko.com>
2020-03-08 23:21:19 +0200

    Turn into Kiwi TCMS plugin
    - does not need MENU_ITEMS and ROOT_URLCONF override anymore
    - does not need to load tcms_tenants in INSTALLED_APPS manually
    
    see
    https://kiwitcms.readthedocs.io/en/latest/plugins/index.html

---
+++ tcms_tenants/tests/test_plugin_setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2020 Alexander Todorov <atodorov@MrSenko.com>
+
+# Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+# pylint: disable=too-many-ancestors
+
+from django.conf import settings
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+from tcms_tenants import menu
+from tcms_tenants.tests import LoggedInTestCase
+
+
+class PluginSetupTestCase(LoggedInTestCase):
+    def test_installed_apps_contains_tcms_tenants(self):
+        self.assertIn('tcms_tenants', settings.INSTALLED_APPS)
+
+    def test_menu_is_updated(self):
+        for name, target in settings.MENU_ITEMS:
+            if name == _('PLUGINS'):
+                for menu_item in menu.MENU_ITEMS:
+                    self.assertIn(menu_item, target)
+
+                return
+
+        self.fail('PLUGINS not found in settings.MENU_ITEMS')
+
+    def test_menu_rendering(self):
+        response = self.client.get(reverse('iframe-navigation'))
+        self.assertContains(response, 'Tenant')
+        self.assertContains(response, 'Authorized users')
