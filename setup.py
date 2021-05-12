588f2ed3dc0d595e295a5e3988dfbe0d46dc5cdf
Alexander Todorov <atodorov@mrsenko.com>
2021-05-12 12:47:42 +0300

    Migrate to Python 3.8

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
     ],
 )
