64290a10811139dc35683271ccad4c22f362aee5
Alexander Todorov <atodorov@mrsenko.com>
2022-04-14 16:48:33 +0300

    Add system check to verify that GroupsBackend is configured

--- tcms_tenants/tests/test_checks.py
+++ tcms_tenants/tests/test_checks.py
@@ -2,12 +2,12 @@ import os
 from unittest.mock import patch
 
 from django.core.checks import Error
+from django.test import TestCase
 
 from tcms_tenants import checks
-from tcms_tenants.tests import LoggedInTestCase
 
 
-class SystemCheckTestCase(LoggedInTestCase):
+class SystemCheckTestCase(TestCase):
     def test_check_passes_when_env_var_is_set(self):
         with patch.dict(os.environ, {
             'KIWI_TENANTS_DOMAIN': 'test.com',
