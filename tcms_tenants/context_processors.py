ae2eee55f5d146792b1b2d3e06a226cd2779e214
Ivajlo Karabojkov <karabojkov@kitbg.com>
2022-07-02 16:11:11 +0300

    Rework according to the code review

--- tcms_tenants/context_processors.py
+++ tcms_tenants/context_processors.py
@@ -1,3 +1,6 @@
+from django.template.loader import render_to_string  # noqa: E402
+
+
 def tenant_navbar_processor(request):
     """
     Provide tenant name for display in navbar
@@ -5,13 +8,7 @@ def tenant_navbar_processor(request):
     tenant_name = request.tenant.schema_name
     if request.tenant.organization:
         tenant_name = request.tenant.organization
-    customized_logo_contents = """
-                    <li style="float:left">
-                    <a>
-                        <span id="tenant">
-                        """ + tenant_name + """
-                        </span>
-                    </a>
-                </li>
-    """
-    return customized_logo_contents
+    customized_logo_contents = render_to_string(
+        "tcms_tenants/tenant_name.html", {"tenant_name": tenant_name}
+    )
+    return {"CUSTOMIZED_LOGO_CONTENTS": customized_logo_contents}
