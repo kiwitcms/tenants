a6083e42ff9885e0a76facc9c72a19c99a250ebe
Ivajlo Karabojkov <karabojkov@kitbg.com>
2022-07-13 00:43:46 +0300

    Clarify namimg condition and fix template

--- tcms_tenants/context_processors.py
+++ tcms_tenants/context_processors.py
@@ -5,11 +5,13 @@ def tenant_navbar_processor(request):
     """
     Provide tenant name for display in navbar
     """
-    tenant_name = request.tenant.schema_name
     if request.tenant.organization:
         tenant_name = request.tenant.organization
-    if request.tenant.name:
+    elif request.tenant.name:
         tenant_name = request.tenant.name
+    else:
+        tenant_name = request.tenant.schema_name
+
     customized_logo_contents = render_to_string(
         "tcms_tenants/tenant_name.html", {"tenant_name": tenant_name}
     )
