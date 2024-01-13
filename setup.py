d5a065d2cc59f595de0cd09a48e9470c4a7313d4
Alexander Todorov <atodorov@otb.bg>
2024-01-14 00:40:14 +0200

    Changelog for v2.6.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def get_install_requires(path):
 
 setup(
     name='kiwitcms-tenants',
-    version='2.5.2',
+    version='2.6.0',
     description='Multi-tenant support for Kiwi TCMS',
     long_description=get_long_description(),
     author='Kiwi TCMS',
